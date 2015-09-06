0d2b226af59c81db436200572127a5b65c2158de
Evan Martinez <evanm21@gmail.com>
2015-09-06 01:41:47 -0400

    fixed footer css

--- src/components/FooterSetrecords.js
+++ src/components/FooterSetrecords.js
@@ -12,19 +12,15 @@ var FooterSetrecords = React.createClass({
 	return(
 
 	
-	<div className="footer  flex-zero">
-		<div className="  flex-row  iconContainer">
-	 		<div className="flex-row   icons">
-			    <a className="click" id="contact">Contact Us</a>
-			    <a className="click" id="about" href="https://setmine.com/about">About</a>
-			    <a className="click" id="dmca">DMCA Notice</a>
-			    <i className="fa fa-copyright"> Setmine. 2015</i>
-			</div>    
-		</div>
+		<footer className="flex-row">
+			<div className="flex-row company-links flex-fixed">
+				    <a className="click" id="contact">Contact Us</a>
+				    <a className="click" id="about" href="https://setmine.com/about">About</a>
+				    <a className="click" id="dmca">DMCA Notice</a>
+				    <i className="fa fa-copyright"> Setmine. 2015</i>
+			</div>
 
-		<div className=" SocialLinks  flex-row iconContainer ">
-			<div className=" spacingIcons icons ">	
-			 
+			<div className="flex-row social-links flex-fixed">			 
 	        	<a href="https://www.facebook.com/SetmineApp">
 	        		<i className="fa fa-2x fa-facebook fa-fw"></i>
 	        	</a>
@@ -33,25 +29,19 @@ var FooterSetrecords = React.createClass({
 	        	</a>
 	        	<a href="https://instagram.com/setmine/">
 	        		<i className="fa fa-2x fa-instagram fa-fw"></i>
-	        	</a>
-	       		
-	    	</div>		
-	    </div>
+	        	</a>	
+		    </div>
 
-	    <div className="  flex-row iconContainer">
-	    	<div className=" flex-row icons sponsorships">
+		    <div className="flex-row sponsorship-links flex-fixed">
 			    <a href="https://teamtreehouse.com">	
 			    	<img src="public/images/treehouse-sponsorship-black-bg.jpg" />
 			    </a>
 			    <a href="https://mixpanel.com/f/partner">
 			    	<img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics"/>
 			    </a>
-			</div>
-	    </div>
-	</div>
-
-	
-		);
+		    </div>
+		</footer>
+	);
     }
  });
 module.exports = FooterSetrecords;
