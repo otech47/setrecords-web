09edea6b17740800e4b669faf7346ea754c2ca54
Evan Martinez <evanm21@gmail.com>
2015-09-27 02:01:42 -0400

    merged with berny's changes

--- src/components/SettingPage.js
+++ src/components/SettingPage.js
@@ -17,25 +17,60 @@ var SettingPage = React.createClass({
 						</div>
 					</div>	
 
-					<div className="flex-row">
+					<div className="flex-row PasswordChange">
 						<div className="flex-columnn flex-fixed">
-							<div></div>
-							<div></div>
-							<div></div>
+							<div className="Artist-name">
+								<h1>Drake</h1>
+								<p>Joined: 8/27/2014</p>
+							</div>
+							<div>
+								<p>New Password</p>
+								<label htmlFor="New Password"/>
+								<input className="newPassword" type="text" placeholder="New Password"/>
+							</div>
+							<div>
+								<p>Confirm New Password</p>
+								<label htmlFor="Confirm New Password"/>
+								<input className="confirmPassword" type="text" placeholder="Confirm New Password"/>
+							</div>
 						</div>
 						
-						<div className="flex-columnn flex-fixed">
-							<div></div>
+						<div className="flex-columnn settingImage flex-fixed">
+							<div className="edit-set-preview">
+			    				<img  id ="settingImage"src="./public/images/settile.png" ></img>
+			    				<button id="change-set">Change Profile Image</button>
+			    			</div>
 						</div>
+
 					</div>
 					
-					<div className="flex-columnn">
-						<div></div>
-						<div></div>
-						<div></div>
-						<div></div>
-						<div></div>
-						<div></div>
+					<div className=" socialmedia flex-columnn">
+						<div>
+							<p>website *</p>
+							<label htmlFor="  "/>
+							<input className="" type="text" placeholder="  "/>
+						</div>
+						<div>
+							<p> soundclooud link</p>
+							<label htmlFor="  "/>
+							<input className="" type="text" placeholder="  "/>
+						</div>
+						<div>
+							<p>youtube link *</p>
+							<label htmlFor="  "/>
+							<input className="" type="text" placeholder="  "/>
+						</div>
+						<div>
+							<p>twitter link</p>
+							<label htmlFor="  "/>
+							<input className="" type="text" placeholder="  "/>
+						</div>
+						<div>
+							<p>facebook link</p>
+							<label htmlFor="  "/>
+							<input className="" type="text" placeholder="  "/>
+						</div>
+						
 					</div>
 				</div>
 			);
