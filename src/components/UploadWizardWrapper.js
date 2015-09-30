9ce6972ab26a67a1190a28e644df2d5fad518d7b
Evan Martinez <evanm21@gmail.com>
2015-09-30 19:23:05 -0400

    upload step 1 complete

--- src/components/UploadWizardWrapper.js
+++ src/components/UploadWizardWrapper.js
@@ -2,12 +2,18 @@ import React from 'react';
 import WizardStepWrapper from './WizardStepWrapper';
 import ProgressBubble from './ProgressBubble';
 import WizardStep1 from './WizardStep1';
+import WizardStep2 from './WizardStep2';
 
 var UploadWizardWrapper = React.createClass({
 	getInitialState: function() {
 		return {
 			type: null,
-			current_step: 1
+			current_step: 1,
+			'1': null,
+			'2': null,
+			'3': null,
+			'4': null,
+			'5': null
 		}
 	},
 	render: function() {
@@ -19,7 +25,7 @@ var UploadWizardWrapper = React.createClass({
 			</div>
 			<p className='step-counter'>Step {this.state.current_step} of 5</p>
 			<div className={'back-arrow' + (this.state.current_step > 1 ? '':' invisible')} onClick={this.stepBackward}>
-				<i className='fa fa-chevron-left'></i> go back
+				<i className='fa fa-chevron-left'></i> back
 			</div>
 			<div className='flex wizard-body'>
 				{this.showWizardStep()}
@@ -27,16 +33,35 @@ var UploadWizardWrapper = React.createClass({
 		</div>
 		);
 	},
-	stepForward: function() {
-		console.log('going forward');
+	stepForward: function(setData) {
+		if (setData) {
+			var self = this;
+			this.setState(setData, function() {
+				console.log(self.state);
+			});
+		} else {
+			console.log("Moving forward (via the forward button presumably...)");
+			var nextStep = this.state.current_step + 1;
+			this.setState({
+				current_step: nextStep
+			});
+		}
 	},
 	stepBackward: function() {
 		console.log('going backward');
+		var previousStep = this.state.current_step - 1;
+		this.setState({
+			current_step: previousStep
+		});
 	},
 	showWizardStep: function() {
 		switch(this.state.current_step) {
 			case 1:
-			return (<WizardStep1 />)
+			return (<WizardStep1 stepForward={this.stepForward}/>);
+			break;
+
+			case 2:
+			return (<WizardStep2 stepForward={this.stepForward} />);
 			break;
 
 			default:
--- src/components/WizardStep1.js
+++ src/components/WizardStep1.js
@@ -4,22 +4,34 @@ var WizardStep1 = React.createClass({
 	render: function() {
 		return (
 			<div className="flex-column wizard-step">
-				<p className='step-info'>What kind of set is this?</p>
-				<div className="flex-row flex step-button-text">
-					<button className="step-button">Live</button>
-					From a festival, concert, or another live venue.
-				</div>
-				<div className="flex-row flex step-button-text">
-					<button className="step-button">Mix</button>
-					A set mixed in a studio, featuring samples from other artists (e.g., a radio show).
-				</div>
-				<div className="flex-row flex step-button-text">
-					<button className="step-button">Album</button>
-					An official, original release.
-				</div>
+				<p className='step-info set-flex'>What kind of set is this?</p>
+				<table className="step-button-text">
+				<tbody>
+					<tr>
+						<td><button name="festival" className="step-button" onClick={this.submitStep}>Live</button></td>
+						<td><p>From a festival, concert, or another live venue.</p></td>
+					</tr>
+					<tr>
+						<td><button name="mix" className="step-button" onClick={this.submitStep}>Mix</button></td>
+						<td><p>A set mixed in a studio, featuring samples from other artists (e.g., a radio show).</p></td>
+					</tr>
+					<tr>
+						<td><button name="album" className="step-button" onClick={this.submitStep}>Album</button></td>
+						<td><p>An official, original release.</p></td>
+					</tr>
+				</tbody>
+				</table>
 			</div>
 		);
-	}
+	},
+
+	submitStep: function(event) {
+		var submission = {};
+		submission['type'] = event.currentTarget.name;
+		submission['current_step'] = 2;
+		submission['1'] = true;
+		this.props.stepForward(submission);
+	},
 });
 
 module.exports = WizardStep1;
