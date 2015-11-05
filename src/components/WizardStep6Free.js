b84716847deaec232f01d180d5ac89070be1a7a7
quinnstinchurchill <quinnpruitt@ufl.edu>
2015-11-04 20:31:44 -0500

    moar upload set

--- src/components/WizardStep6Free.js
+++ src/components/WizardStep6Free.js
@@ -4,9 +4,9 @@ import _ from 'underscore';
 var WizardStep6Free = React.createClass({
 	render: function() {
 		return (
-			<div className="flex-column wizard-step">
+			<div className="flex-column wizard-step" id='WizardStep6Free'>
 				<p className='step-info set-flex'>Select any additional sites to release to, then click continue.</p>
-				<div className="flex-row step-buttons">
+				<div className="flex-row buttons">
 					<div className={"outlet-button flex-column " + (this.props.outlets.indexOf('Soundcloud') >= 0 ? '':'deactivated')} onClick={this.props.toggleOutlet.bind(null, 'Soundcloud')}>
 						<i className='fa fa-soundcloud' />
 						<p>Soundcloud</p>
