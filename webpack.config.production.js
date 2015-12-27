7116a64a2b31de32ff8c8ee27aca5a4d05650202
Oscar Lafarga <otech47@gmail.com>
2015-12-27 18:51:03 -0500

    merge push

---
+++ webpack.config.production.js
@@ -0,0 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+var buildPath = path.resolve(__dirname, 'public');
+var mainPath = path.resolve(__dirname, 'src', 'index.jsx');
+
+module.exports = {
+    entry: {
+        setrecords: mainPath
+    },
+    output: {
+        path: buildPath,
+        filename: '[name]-bundle.js',
+        pathinfo: true,
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: ['', '.jsx', '.es6', '.js', '.scss'],
+        moduleDirectories: ['node_modules']
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        inject: 'body'
+    })],
+    devtool: 'cheap-source-map',
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
