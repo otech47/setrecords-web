4bb990a0260862ef7ba05719698cb9676491c5e3
Evan Martinez <evanm.design@gmail.com>
2016-05-09 19:14:10 -0400

    Revert "added view container component to set up better flex grid"
    This reverts commit a779001d9c39ded5800fb1e5dd184797e1d8b6e5.

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,6 @@ import SettingsEditor from './components/SettingsEditor';
 import Contact from './components/Contact';
 import UploadSetWizard from './components/UploadSetWizard';
 import UploadTrackWizard from './components/UploadTrackWizard';
-import ViewContainer from './components/ViewContainer';
 
 
 var bodyMount = document.getElementById('body-mount-point');
@@ -21,15 +20,13 @@ var routes = (
     <Route path='/' component={App} >
         <IndexRoute component={LoginPage} />
 
-        <Route component={ViewContainer} >
-            <Route path='dashboard' component={Dashboard} />
-            <Route path='content' component={ContentView} />
-            <Route path='edit/:id' component={SetEditor} />
-            <Route path='account' component={SettingsEditor} />
-            <Route path='contact' component={Contact} />
-            <Route path='upload-set' component={UploadSetWizard} />
-            <Route path='upload-track' component={UploadTrackWizard} />
-        </Route>
+        <Route path='dashboard' component={Dashboard} />
+        <Route path='content' component={ContentView} />
+        <Route path='edit/:id' component={SetEditor} />
+        <Route path='account' component={SettingsEditor} />
+        <Route path='contact' component={Contact} />
+        <Route path='upload-set' component={UploadSetWizard} />
+        <Route path='upload-track' component={UploadTrackWizard} />
     </Route>
 );
 
